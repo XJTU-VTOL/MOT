4ac13537c7603fcc0f6356232a6325c89729c83f
lyh6560 <photo12112112121@gmail.com>
2021-08-03 14:49:07 +0800

    a test case for our dataset and modified data loader

---
+++ test/test_our_dataset.py
@@ -0,0 +1,34 @@
+import argparse
+import json
+import time
+from time import gmtime, strftime
+import unittest
+from torchvision.transforms import transforms as T
+import torch
+from dataset.dataset import *
+class Test_RealDataset(unittest.TestCase):
+    def test_D(self):
+        trainset_paths ={"CA003":"D://multi-object//MOT//data//CA003.train"}
+        dataset_root = "D://multi-object//MOT"
+
+
+        transforms = T.Compose([T.ToTensor()])
+        # Get dataloader
+        dataset = JointDataset(dataset_root, trainset_paths, (1088,608), augment=True, transforms=transforms)
+        dataloader = torch.utils.data.DataLoader(dataset, batch_size=1, shuffle=True,
+
+                                              num_workers=1, pin_memory=True, drop_last=True, collate_fn=collate_fn)
+
+        '''
+        JointDataset returns imgs, labels, img_path, (h, w)
+        labels :[class] [identity] [x_center] [y_center] [width] [height]
+        '''
+        for i,  (imgs, targets, _, _, targets_len) in enumerate(dataloader):
+
+            print(targets)
+            #imgs, targets, _, _, targets_len=j
+
+
+if __name__ == '__main__':
+    # verbosity=*：默认是1；设为0，则不输出每一个用例的执行结果；2-输出详细的执行结果
+    unittest.main(verbosity=2)
