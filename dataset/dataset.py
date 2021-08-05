88cbcac125123ccdba8a5883da2c103a2ba76427
lyh6560 <photo12112112121@gmail.com>
2021-08-05 15:46:13 +0800

    correct xywh

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -178,7 +178,7 @@ class LoadImagesAndLabels:  # for training
         return img, labels, img_path, (h, w)
     '''
     img: images in tensor form if transformed else in np array
-    labels:cls,id,left_up_x,left_up_y,right_down_x,right_down_y in list 
+    labels:cls,id,x,y,w,h in list 
     img_path:the path for the output images
     (h,w): the height and width for the output images
     '''
